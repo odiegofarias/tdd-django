6dffdd7b48383ef71c4d23bde6ff0a47af8291a2
odiegofarias <diego.farias.fernandes@gmail.com>
2023-01-03 21:32:16 -0300

    Associando um autor ao post utilizando a biblioteca model-bakery

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 """
@@ -10,12 +11,15 @@ from django.urls import reverse
         -created_at: datetime
         -updated_at: datetime
 """
+User = get_user_model()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self) -> str:
--- posts/tests.py
+++ posts/tests.py
@@ -3,6 +3,11 @@ from .models import Post
 from http import HTTPStatus
 from django.urls import resolve, reverse
 from . import views
+from model_bakery import baker
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 # Create your tests here.
@@ -13,22 +18,23 @@ class PostModelTest(TestCase):
         self.assertEqual(posts, 0)
 
     def test_string_representation_of_objects(self):
-        post = Post.objects.create(
-            title='Test Post',
-            body='Test Body',
-        )
+        post = baker.make(Post)
 
         self.assertEqual(str(post), post.title)
+        self.assertTrue(isinstance(post, Post))
 
 
 class HomepageTest(TestCase):
     def setUp(self) -> None:
+        self.user = baker.make(User)
         post1 = Post.objects.create(
+            author=self.user,
             title='sample post 1',
             body='It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         )
 
         post2 = Post.objects.create(
+            author=self.user,
             title='sample post 2',
             body='It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         )
@@ -56,9 +62,11 @@ class HomepageTest(TestCase):
 
 class DetailPageTest(TestCase):
     def setUp(self) -> None:
+        self.user = baker.make(User)
         self.post = Post.objects.create(
             title='Learn Javascript in the 23 hour course',
-            body='this is a beginner course on JS'
+            body='this is a beginner course on JS',
+            author=self.user,
         )
 
     def test_detail_page_returns_correct_response(self):
@@ -73,3 +81,22 @@ class DetailPageTest(TestCase):
         self.assertContains(response, self.post.title)
         self.assertContains(response, self.post.body)
         # self.assertContains(response, self.post.created_at)
+
+
+class PostAuthorTest(TestCase):
+    def setUp(self) -> None:
+        self.user = baker.make(User)
+        self.post = Post.objects.create(
+            title='Test title',
+            body='Test body',
+            author=self.user,
+        )
+
+
+    def test_author_and_post_is_instance_of_user_model(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_post_belongs_to_user(self):
+        self.assertTrue(hasattr(self.post, 'author'))
+        self.assertEqual(self.post.author, self.user)
+        
