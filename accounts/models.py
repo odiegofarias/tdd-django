6c9e790832c7dc7ed79c964f5b75549f40ef7ba2
odiegofarias <diego.farias.fernandes@gmail.com>
2023-01-08 22:15:19 -0300

    Criando função para imagem padrão ao criar o usuário

--- accounts/models.py
+++ accounts/models.py
@@ -14,7 +14,7 @@ User = get_user_model()
 """
 class Profile(models.Model):
     bio = models.TextField()
-    profile_imagem = models.URLField(max_length=500)
+    profile_image = models.URLField(max_length=500)
     address = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
--- accounts/signals.py
+++ accounts/signals.py
@@ -2,12 +2,13 @@ from .models import Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from .utils import create_avatar
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, profile_image=create_avatar(instance.email))
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
